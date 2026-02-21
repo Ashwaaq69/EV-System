c06555422a8573dff0a2128bc437256bbfb3966c
Ashwaaq69 <ashuuMohaz2@gmail.com>
2026-02-20 23:15:45 -0800

    fix: force HTTPS more aggressively to resolve Mixed Content on production

--- citrine-dashboard/app/Providers/AppServiceProvider.php
+++ citrine-dashboard/app/Providers/AppServiceProvider.php
@@ -20,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || config('app.env') === 'docker') {
+        if (config('app.env') === 'production' || config('app.env') === 'docker' || env('FORCE_HTTPS') === true) {
             \Illuminate\Support\Facades\URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', 'on');
         }
         Vite::prefetch(concurrency: 3);
     }
